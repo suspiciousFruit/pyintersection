f1ce615b032916571710ccaf4aa10da87ba0f72a
Alexander K <docspochta90@gmail.com>
2021-12-08 16:42:39 +0300

    setup.py changed

--- setup.py
+++ setup.py
@@ -1,15 +1,36 @@
 # The script that will build the module
+# from distutils.core import setup, Extension
 
 from setuptools import setup, Extension
-# from distutils.core import setup, Extension
-import ospath
+import sysconfig
+import sys
+from pathlib import Path
+
+def get_include_dirs():
+    paths = sysconfig.get_paths()
+    python_include = paths['include']
+    numpy_include = paths['purelib'] + str(Path('/numpy/core/include/numpy'))
+    return python_include, numpy_include
+
+def get_extra_compile_args():
+    return ['/std:c++17'] if sys.platform == 'win32' else ['-std=c++17']
+
+# def get_platform_postfix():
+#     if sys.platform == 'win32':
+#         return '\\numpy\\core\\include\\numpy'
+#     else:
+#         return '/numpy/core/include/numpy'
+
+PYTHON_INCLUDE, NUMPY_INCLUDE = get_include_dirs()
+EXTRA_COMPILE_ARGS = get_extra_compile_args()
+
 
 module = Extension('pyintersection',
                     sources = ['src/pymodule/main.cpp'],
-                    include_dirs = [ospath.PYTHON_INCLUDE, ospath.NUMPY_INCLUDE],
-                    extra_compile_args = ["/std:c++17"])
+                    include_dirs = [PYTHON_INCLUDE, NUMPY_INCLUDE],
+                    extra_compile_args = EXTRA_COMPILE_ARGS)
 
 setup(name = 'pyintersection',
         version = '1.0',
-        description = 'This is a demo package',
+        description = 'Pyintersection package',
         ext_modules = [module])
